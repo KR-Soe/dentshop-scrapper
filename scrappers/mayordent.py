95d6e3097be4de38ea161645d7f6469a9896f163
Sebastian Real <seba.real.a@gmail.com>
2021-01-04 20:03:09 -0300

    added default values

--- scrappers/mayordent.py
+++ scrappers/mayordent.py
@@ -14,7 +14,7 @@ class Mayordent(scrapy.Spider):
     def start_requests(self):
         self.connection = make_mongo_conn()
 
-        for page_number in range(1, 999):
+        for page_number in range(1, 100):
             url = f'https://www.mayordent.cl/page/{page_number}/?s=+&post_type=product'
             yield Request(url, callback=self.parse_list, errback=self.do_nothing)
 
@@ -34,10 +34,8 @@ class Mayordent(scrapy.Spider):
 
     def _parse_detail(self, response):
         product_title = response.css('.product_title.entry-title::text').get()
-        internet_price = self._get_number(response.css('.woocommerce-Price-amount.amount > bdi::text').get())
-        normal_price = self._get_number(response.css('.woocommerce-product-details__short-description > p::text').get())
+        internet_price = self._get_number(response.css('.summary.entry-summary .woocommerce-Price-amount.amount > bdi::text').get())
         stock = self._get_number(response.css('.stock.in-stock::text').get())
-
         sku = response.css('.sku_wrapper > .sku::text').get()
         category = response.css('.posted_in > a::text').getall()
         image = response.css('.wp-post-image.thb-ignore-lazyload::attr(src)').get()
@@ -46,7 +44,7 @@ class Mayordent(scrapy.Spider):
         output = {
             'title': product_title,
             'internetPrice': int(internet_price),
-            'normalPrice': int(normal_price),
+            'normalPrice': None,
             'stock': int(stock),
             'sku': sku,
             'category': category,
@@ -55,7 +53,6 @@ class Mayordent(scrapy.Spider):
         }
 
         self.connection.dentshop.mayordent.insert_one(output)
-
 
     def _get_number(self, text):
         if text is None:

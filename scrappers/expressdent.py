951702c6bca181ee3b1672dae56c53ef33d8698a
Sergio <soe.vera.a@gmail.com>
2021-01-05 20:25:50 -0300

    expressdent fixed scrapping all categories

--- scrappers/expressdent.py
+++ scrappers/expressdent.py
@@ -1,5 +1,6 @@
 import re
 import scrapy
+import json
 from scrapy.http import Request
 from scrapy.crawler import CrawlerProcess
 from utils.connection import make_mongo_conn
@@ -10,38 +11,33 @@ class Expressdent(scrapy.Spider):
 
     def start_requests(self):
         self.connection = make_mongo_conn()
-        start_url = 'https://expressdent.cl/'
+        with open('./scrappers/inputs/expressdent.json', 'r') as file:
+            start_urls = json.load(file)
 
-        yield Request(start_url, callback=self.parse)
+        for url in start_urls:
+            yield Request(url, callback=self.parse_list)
 
-    def parse(self, response):
-        last_page = [
-            int(num)
-            for num in response.css('.page-numbers::text').getall()
-            if num.isnumeric()
-        ][-1]
-
-        for i in range(1, last_page + 1):
-            item = response.css('.ht-product-image > a[href]::attr(href)').getall()
-
-            for link in item:
-                yield Request(link, callback=self._parse_detail)
+    def parse_list(self, response):
+        items = response.css('.products > .entry')
+        for item in items:
+            link = item.css('a::attr(href)').get()
+            yield Request(link, callback=self._parse_detail)
 
 
     def _parse_detail(self, response):
         product_title = response.css('.single-post-title.product_title.entry-title::text').get()
-        normal_price = self._get_number(response.css('.price::text').get())
-        internet_price = self._get_number(response.css('.price::text').get())
-        stock = self._get_number(response.css('.stock in-stock::text').get())
+        normal_price = self._get_number(response.css('.summary.entry-summary > .price > span > bdi::text').get())
+        internet_price = self._get_number(response.css('.summary.entry-summary > .price > span > bdi::text').get())
+        stock = self._get_number(response.css('.stock.in-stock::text').get())
         sku = response.css('.sku_wrapper > .sku::text').get()
         category = response.css('.posted_in > a::text').getall()
-        image = response.css('.zoomImg::attr(src)').get()
+        image = response.css('.woocommerce-product-gallery__image > a::attr(href)').get()
 
         output = {
             'title': product_title,
-            'internetPrice': int(internet_price, 10),
-            'normalPrice': int(normal_price, 10),
-            'stock': int(stock, 10),
+            'internetPrice': int(internet_price),
+            'normalPrice': int(normal_price),
+            'stock': int(stock),
             'sku': sku,
             'category': category,
             'image': image,
@@ -49,7 +45,6 @@ class Expressdent(scrapy.Spider):
         }
 
         self.connection.dentshop.expressdent.insert_one(output)
-
 
     def _get_number(self, text):
         result = re.sub(r'\D*', '', text)

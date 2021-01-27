36e8705e1d97c95f8c38e3e961e115166f0deaff
Sebastian Real <seba.real.a@gmail.com>
2021-01-26 21:30:35 -0300

    added revenue checker to round the prices
    the current prices will be round using 0, 500, 900 according to the case
    
    this fixes #22

--- src/services/pricing.js
+++ src/services/pricing.js
@@ -1,7 +1,23 @@
 const service = {
   _revenue: 1.5,
   calculatePriceWithRevenue(originalPrice) {
-    return originalPrice * this._revenue;
+    const precalculatedRevenue = originalPrice * this._revenue;
+    return this._adjustRevenue(precalculatedRevenue);
+  },
+  _adjustRevenue(initialRevenue) {
+    const basePrice = Math.trunc(initialRevenue / 1000);
+    const initialDelta = initialRevenue % 1000;
+
+    const delta = [0, 500, 900].reduce((acc, rest) => {
+      if (Math.abs(rest - initialDelta) <= acc.delta) {
+        acc.newRevenue = rest;
+        acc.delta = Math.abs(rest - acc.delta);
+      }
+
+      return acc;
+    }, { newRevenue: null, delta: initialDelta });
+
+    return (basePrice) * 1000 + delta.newRevenue;
   }
 };
 

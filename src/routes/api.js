8c3d3f239d772bf666c1137b0e778bbd266accb4
Sebastian Real <seba.real.a@gmail.com>
2021-02-22 11:40:45 -0300

    removed CSV section from code

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,10 +1,5 @@
-const multer = require('multer');
 const router = require('express').Router();
-const upload = multer({
-  dest: '/tmp/dentshop/'
-});
 
 router.post('/auth', require('../controllers/authenticate'));
-router.post('/csv', upload.single('jumpseller'), require('../controllers/csv'));
 
 module.exports = router;

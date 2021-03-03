aebd757d7a77eeb6d5d753459531b70c31e7ca06
Sebastian Real <seba.real.a@gmail.com>
2021-03-02 23:13:52 -0300

    hotfix: removed sandbox limit restriction to be allowed to run on D.O.

--- scrappers/utils/makeBrowser.js
+++ scrappers/utils/makeBrowser.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 const makeNewBrowser = async (throttle = 0, headless = true, devtools = true) => {
-  const options = { headless , slowMo: throttle, devtools };
+  const options = { headless , slowMo: throttle, devtools, args: ['--no-sandbox'] };
   const browser = await puppeteer.launch(options);
   return browser;
 };
